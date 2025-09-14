176003169f0116b6a15219de37f8857dbf7f527c
Arfzz <ariefadhityaramadhan@gmail.com>
2025-09-15 00:53:07 +0700

    fix dir

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/belajar-react/',
+  base: '/learn-react/',
   plugins: [react()],
   resolve: {
     alias: {
